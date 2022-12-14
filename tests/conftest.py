45239f1f190f5c9d2d19c580f65a8e2bdf73b80d
a.privalov <a.privalov@jump.finance>
2022-12-14 19:25:25 +0300

    updated .gitignore's directory, added size of the browser window

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,5 +5,7 @@ import pytest
 @pytest.fixture(scope='function', autouse=True)
 def form_management():
     browser.config.base_url = 'https://demoqa.com'
+    browser.config.window_width = 1920
+    browser.config.window_height = 1080
     yield
     browser.quit()
